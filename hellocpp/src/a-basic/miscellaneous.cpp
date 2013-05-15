2755d87cc584a3b6eb4e698abd11237f4914ad13
slieer <zxiaobi@hotmail.com>
2013-05-15 19:06:49 +0800

    cpp & nextstarmarket admin doc

--- hellocpp/src/a-basic/miscellaneous.cpp
+++ hellocpp/src/a-basic/miscellaneous.cpp
@@ -7,18 +7,18 @@ static void _MyNew(int* p) {
 }
 
 /**
-static int f(int a) {
-	if (a > 0) {
-		return 0;
-	}
-	return a;
-}
-*/
+ static int f(int a) {
+ if (a > 0) {
+ return 0;
+ }
+ return a;
+ }
+ */
 static int like_f(int a) {
 	return (a - -a) / 2;
 }
 
-static void callByVal(){
+static void callByVal() {
 	int*p = NULL;
 	bool isFailed = false;
 	_MyNew(p);
@@ -26,13 +26,39 @@ static void callByVal(){
 	if (p == NULL)
 		isFailed = true;
 
-	cout << "isFailed" << isFailed << endl;
+	cout << "isFailed:" << isFailed << endl;
 }
+
+class Poin {
+public:
+	static void func() {
+		/*
+		 * ISO C++ forbids incrementing a pointer of type 'void*' [-fpermissive]
+		 * */
+//		void* p = NULL;
+//		p++;
+		char* q = NULL;
+		q++;
+		int* r = NULL;
+		r++;
+	}
+
+	static void f(int* p) {
+	}
+	static void f1(int p[]) {
+	}
+};
 
 void miscellaneous() {
 	cout << "like_f(1):" << like_f(1) << endl;
 	cout << "like_f(-20):" << like_f(-20) << endl;
 
 	callByVal();
+
+	Poin p;
+	p.func();
+	p.f(NULL);
+	p.f1(NULL);
+
 }
 
--- hellocpp/src/a-basic/virtual_test.cpp
+++ hellocpp/src/a-basic/virtual_test.cpp
@@ -20,9 +20,21 @@ class D: public B, public C {
 
 #include <iostream>
 using namespace std;
+
+/*
+1、空类、单一继承的空类、多重继承的空类所占空间大小为：1（字节，下同）；
+2、一个类中，虚函数本身、成员函数（包括静态与非静态）和静态数据成员都是不占用类对象的存储空间的；
+3、因此一个对象的大小≥所有非静态成员大小的总和；
+4、当类中声明了虚函数（不管是1个还是多个），那么在实例化对象时，编译器会自动在对象里安插一个指针vPtr指向虚函数表VTable；
+5、虚承继的情况：由于涉及到虚函数表和虚基表，会同时增加一个（多重虚继承下对应多个）vfPtr指针指向虚函数表vfTable和一个vbPtr指针指向虚基表vbTable，这两者所占的空间大小为：8（或8乘以多继承时父类的个数）；
+6、在考虑以上内容所占空间的大小时，还要注意编译器下的“补齐”padding的影响，即编译器会插入多余的字节补齐；
+7、类对象的大小=各非静态数据成员（包括父类的非静态数据成员但都不包括所有的成员函数）的总和+ vfptr指针(多继承下可能不止一个)+vbptr指针(多继承下可能不止一个)+编译器额外增加的字节。
+ * */
 void virtual_test() {
-	cout << "sizeof(A) :" <<sizeof(A) << endl;
-	cout << "sizeof(B) :" <<sizeof(B) << endl;
-	cout << "sizeof(C) :" <<sizeof(C) << endl;
-	cout << "sizeof(D) :" <<sizeof(D) << endl;
+	cout<< sizeof (int) <<endl;
+
+	cout << "sizeof(A) :" <<sizeof(A) << endl; //1
+	cout << "sizeof(B) :" <<sizeof(B) << endl; //4
+	cout << "sizeof(C) :" <<sizeof(C) << endl; //4
+	cout << "sizeof(D) :" <<sizeof(D) << endl; //8
 }
